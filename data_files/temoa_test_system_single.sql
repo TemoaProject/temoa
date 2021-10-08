5b084df3e28799691217aed17822b1b812324126
Umang Jain <umang@samitsolutions.com>
2021-10-08 16:06:27 +0530

    data files safe changes

--- data_files/temoa_test_system_single.sql
+++ data_files/temoa_test_system_single.sql
@@ -329,12 +329,6 @@ CREATE TABLE IF NOT EXISTS "Output_Costs" (
 	FOREIGN KEY("vintage") REFERENCES "time_periods"("t_periods"),
 	FOREIGN KEY("tech") REFERENCES "technologies"("tech")
 );
-CREATE TABLE "Output_Duals" (
-	"constraint_name"	text,
-	"scenario"	text,
-	"dual"	real,
-	PRIMARY KEY("constraint_name","scenario")
-);
 CREATE TABLE IF NOT EXISTS "Output_Curtailment" (
 	"regions"	text,
 	"scenario"	text,
