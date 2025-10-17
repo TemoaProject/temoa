90e32341be82056c0cd1159d2e953beae1ea1fd1
ParticularlyPythonicBS <anil@anilr.dev>
2025-10-17 12:46:06 -0400

    fixing typing in definitions.py

--- definitions.py
+++ definitions.py
@@ -2,26 +2,19 @@
 top-level folder to provide project-wide definitions & references
 """
 
-# Written by:  J. F. Hyink
-# jeff@westernspark.us
-# https://westernspark.us
-
-# Created on:  6/28/23
-
 import os
 from pathlib import Path
-from typing import cast
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-OUTPUT_PATH = None
+OUTPUT_PATH: Path | None = None
 
 
 def get_OUTPUT_PATH() -> Path:
     if OUTPUT_PATH is None:
         raise RuntimeError('Output path not yet defined')
     # Use cast to help mypy understand the type
-    return cast(Path, OUTPUT_PATH)  # type: ignore[unreachable]
+    return OUTPUT_PATH
 
 
 def set_OUTPUT_PATH(path: Path) -> None:
