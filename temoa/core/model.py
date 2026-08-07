8965e203b51bf1205006544a97b462c66ebe689e
Davey Elder <iandavidelder@gmail.com>
2026-08-07 15:41:04 -0400

    Wrap eos import in extension check
    Signed-off-by: Davey Elder <iandavidelder@gmail.com>

--- temoa/core/model.py
+++ temoa/core/model.py
@@ -39,9 +39,6 @@ from temoa.components import (
     technology,
     time,
 )
-from temoa.extensions.economies_of_scale.core.model import (
-    register_early_eos_components,
-)
 from temoa.extensions.framework import apply_model_extension_hooks, resolve_extension_specs
 from temoa.model_checking.validators import (
     no_slash_or_pipe,
@@ -567,7 +564,10 @@ class TemoaModel(AbstractModel):
         self.cost_invest = Param(self.cost_invest_rtv)
 
         # Inject cost_invest_eos extension components prior to loan params, if active
-        register_early_eos_components(self)
+        if 'eos' in self.enabled_extensions:
+            import temoa.extensions.economies_of_scale.core.model as eos
+
+            eos.register_early_eos_components(self)
 
         self.default_loan_rate = Param(domain=NonNegativeReals)
         self.loan_rate = Param(
